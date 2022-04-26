8b8be20ff596aecb803380eeb1a1bdc057a3c34b
Guillermo Rodriguez <gui_rod_dallas@hotmail.com>
2022-04-26 12:46:02 -0400

    Fix to source & sink pairs

--- custom_topology.py
+++ custom_topology.py
@@ -12,7 +12,7 @@ class DDoSTopology(Topo):
         with open(os.path.join(os.getcwd(), IN_FOLDER, IN_FILE), 'r') as _network_paths:
             # Build node connections
             for _entry in _network_paths:
-                _source, _sink = int(_entry[0]), int(_entry[1])
+                _source, _sink = _entry.split(',')[0], _entry.split(',')[1]
                 
                 if _source in _network_matrix.keys():
                     _network_matrix[_source].append(_sink)
@@ -24,6 +24,6 @@ class DDoSTopology(Topo):
             _switch = self.addSwitch(str(_key))
 
             for _host in _value:
-                self.addLink(self.addHost(str(_host)), _switch)
+                self.addLink(self.addHost(_host), _switch)
 
 topos = { 'ddostopology': ( lambda: DDoSTopology() ) }
